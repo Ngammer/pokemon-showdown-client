ce7d558d68588082b3eaea29ed803122ce0a3f7a
Ngammer <cnicolas810@gmail.com>
2025-01-05 16:37:56 -0300

    Update battle.js

--- dist/battle.js
+++ dist/battle.js
@@ -3061,17 +3061,16 @@ class Battle {
                 this.activateAbility(poke, fromeffect);
                 let minTimeLeft = 5;
                 let maxTimeLeft = 0;
-                if (effect.id.endsWith('terrain')) {
-                    for (let i = this.pseudoWeather.length - 1; i >= 0; i--) {
-                        let pwID = toID(this.pseudoWeather[i][0]);
-                        if (pwID.endsWith('terrain')) {
-                            this.pseudoWeather.splice(i, 1);
-                            continue;
-                        }
-                    }
-                    if (this.gen > 6)
-                        maxTimeLeft = 8;
-                }
+                if (effect.id.endsWith('terrain') || effect.id.endsWith('room') || effect.id.endsWith('gravity')) {
+						for (let i = this.pseudoWeather.length - 1; i >= 0; i--) {
+							let pwID = toID(this.pseudoWeather[i][0]);
+							if (pwID.endsWith('terrain')) {
+								this.pseudoWeather.splice(i, 1);
+								continue;
+							}
+						}
+						if (this.gen > 6) maxTimeLeft = 8;
+					}
                 if (kwArgs.persistent)
                     minTimeLeft += 2;
                 this.addPseudoWeather(effect.name, minTimeLeft, maxTimeLeft);
